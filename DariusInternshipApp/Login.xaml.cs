3c778b5f7debea3823ee35728f0af3b00d5ca913
dariusvlok30 <dariusvlok30@gmail.com>
2024-10-23 10:19:10 +0200

    Adding login window
    Adding login window

--- DariusInternshipApp/Login.xaml.cs
+++ DariusInternshipApp/Login.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,56 @@ namespace DariusInternshipApp
     /// </summary>
     public partial class Login : Window
     {
+        private string connectionStringHardcoded = @"Server=localhost\MSSQLSERVER01;Database=DariusInternship;Trusted_Connection=True;";
+
         public Login()
         {
             InitializeComponent();
         }
+        private void txtLoginUsername_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            DataTable dt = new DataTable();
+            string hashedPassword = "";
+            using (SqlConnection connection = new SqlConnection(connectionStringHardcoded))
+            {
+                using (SqlCommand command = new SqlCommand("sp_get_userPassword", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("username", txtLoginUsername.Text);
+
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    dt.Load(reader);
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        hashedPassword = row["Password"].ToString();
+                    }
+                }
+            }
+
+            if (hashedPassword != "" && BCrypt.Net.BCrypt.Verify(pwdLoginPassword.Password, hashedPassword))
+            {
+                MainWindow  mainWindow = new MainWindow();
+                this.Close();
+                mainWindow.Show();
+            }
+            else if (hashedPassword == "")
+            {
+                MessageBox.Show("User doesnt exist.");
+            }
+            else
+            {
+                MessageBox.Show("Couldnt log in");
+            }
+        }
     }
 }
+
+
+
+
